95cf9cf5f3b8b8569e25cab12c6db6cf6c5a81be
Antonio Rodrigues <netodnet@gmail.com>
2024-01-01 19:55:02 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+using DesafioPOO.Models;
+
+Nokia N1 = new Nokia("99774331", "NokiaG21", "123123", 128);
+Console.WriteLine($"Usando um Nokia: ");
+N1.Ligar();
+
+N1.ReceberLigacao();
+
+N1.InstalarAplicativo("Whatsapp");
+
+Iphone I1 = new Iphone("88564526", "Iphone14", "321321", 256);
+
+Console.WriteLine("\nUsando um iphone: ");
+
+I1.ReceberLigacao();
+
+I1.InstalarAplicativo("Safari");
+
